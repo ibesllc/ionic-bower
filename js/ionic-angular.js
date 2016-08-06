7d34c3836659ec9e0affefd7062c1822f8b5825b
Mahendra Soni <mmahendra.soni@gmail.com>
2016-08-06 13:48:53 +0530

    Fix slidebox align in RTL.

--- js/ionic-angular.js
+++ js/ionic-angular.js
@@ -12639,7 +12639,8 @@ function($timeout, $compile, $ionicSlideBoxDelegate, $ionicHistory, $ionicScroll
         },
         onDragEnd: function() {
           freezeAllScrolls(false);
-        }
+        },
+        isRTL: $ionicHistory.isRTL()
       });
 
       function freezeAllScrolls(shouldFreeze) {
